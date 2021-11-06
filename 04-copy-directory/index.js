2720e80b45bbd5ffe1870589a0aff5d8d2b11204
m.filionchyk <maria.filenchik@yandex.by>
2021-11-06 15:49:58 +0300

    feat: add 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,18 @@
+const fs = require('fs');
+const { readdir, mkdir, copyFile } = require('fs/promises');
+const path = require('path');
+const folderName = path.resolve(__dirname, 'files');
+const folderNameCopy = path.resolve(__dirname, 'files-copy');
 
+const copyDir = async (folderName, folderNameCopy) => {
+  fs.access(folderNameCopy, err => {
+    if(err){
+      mkdir(folderNameCopy, { recursive: true })
+      .catch(err => console.log(err)) 
+    } 
+  })
+  await readdir(folderName)
+    .then(files => files.map((file, i) => copyFile(`${folderName}/${file}`, `${folderNameCopy}/${file}`)))
+    .catch(err => console.log(err));
+}
+copyDir(folderName, folderNameCopy);
